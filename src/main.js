437ea7577ca89f1d38d5db28c149aface2d9163f
ferry ramadhani <ramadhaniferry1996@gmail.com>
2025-06-28 16:30:20 +0700

    init repo

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Typed from "typed.js";
+
+const myTyped = new Typed("#greeting", {
+  strings: ["Full Stack Developer", "Software Architecture", "Ethical Hacking"],
+  typeSpeed: 5,
+  backSpeed: 7,
+  loop: true,
+});
